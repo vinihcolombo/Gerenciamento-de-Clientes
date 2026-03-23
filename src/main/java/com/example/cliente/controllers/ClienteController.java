d6fe2dbfd4d6361bd1639243b5ce6ff34f3af779
flavioaugustto <flavioaugustosilv@gmail.com>
2026-03-23 11:06:48 -0300

    Finalização

--- src/main/java/com/example/cliente/controllers/ClienteController.java
+++ src/main/java/com/example/cliente/controllers/ClienteController.java
@@ -1,4 +1,36 @@
 package com.example.cliente.controllers;
 
+import com.example.cliente.models.ClienteModel;
+import com.example.cliente.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService service;
+
+    @PostMapping
+    public ClienteModel salvar(@RequestBody ClienteModel cliente) {
+        return service.salvar(cliente);
+    }
+
+    @GetMapping
+    public List<ClienteModel> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ClienteModel buscar(@PathVariable Long id) {
+        return service.buscar(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
--- src/main/java/com/example/cliente/services/ClienteService.java
+++ src/main/java/com/example/cliente/services/ClienteService.java
@@ -1,4 +1,31 @@
 package com.example.cliente.services;
 
+import com.example.cliente.models.ClienteModel;
+import com.example.cliente.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    public ClienteModel salvar(ClienteModel cliente) {
+        return repository.save(cliente);
+    }
+
+    public List<ClienteModel> listar() {
+        return repository.findAll();
+    }
+
+    public ClienteModel buscar(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
